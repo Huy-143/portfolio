e845d7a8a4502b0563b0d7edb12ac0bbf1552a2e
Huy <127241570+Huy-143@users.noreply.github.com>
2024-09-30 23:43:16 +0200

    Delete Portfolio.sql

--- Portfolio.sql
+++
@@ -1,9 +0,0 @@
--- What is the total amount each customer spent at the restaurant?
-
-SELECT
-	customer_id,
-    SUM(price) AS total_spent
-FROM sales s
-JOIN menu m ON s.product_id = m.product_id
-GROUP BY customer_id
-
